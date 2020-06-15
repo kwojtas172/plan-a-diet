95674f5e7b0b6f21e3ea294d436a0c5960032894
kwoy <gringo.first@gmail.com>
2020-06-15 11:14:53 +0200

    sections: about app, form to subscription, about author, structure html and style in scss, update landing page, main.scss, added folder images

--- src/js/components/LandingPage.js
+++ src/js/components/LandingPage.js
@@ -1,9 +1,16 @@
 import React from "react";
+import AboutApp from "./AboutApp";
+import FormToSubscription from "./FormToSubscription";
+import AboutAuthor from "./AboutAuthor";
 
 export default class LandingPage extends React.Component {
     render() {
         return (
-            <div className="wrapper"></div>
+            <div className="wrapper">
+                <AboutApp />
+                <FormToSubscription />
+                <AboutAuthor />
+            </div>
         )
     }
 }
