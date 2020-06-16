f9f0251f658cc8e2926a564934b68f61deebc806
Agata Nakonieczna <agnakonieczna@gmail.com>
2020-06-16 18:19:25 +0200

    first main section of app added

--- src/js/components/LandingPage.js
+++ src/js/components/LandingPage.js
@@ -6,7 +6,6 @@ import AboutApp from "./AboutApp";
 import FormToSubscription from "./FormToSubscription";
 import AboutAuthor from "./AboutAuthor";
 import Footer from "./Footer";
-import AppMainSection1 from "./AppMainSection1";
 
 export default class LandingPage extends React.Component {
     render() {
@@ -19,7 +18,6 @@ export default class LandingPage extends React.Component {
                 <FormToSubscription />
                 <AboutAuthor />
                 <Footer />
-                <AppMainSection1 />
             </div>
         )
     }
---
+++ src/js/components/SecondAppSection.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default class SecondAppSection extends React.Component {
+    render() {
+        return(
+            <div>Second App Section</div>
+        )
+    }
+}
