fa01d2684b7255d8f11a9f7284c8c8ce1e272b4a
kwoy <gringo.first@gmail.com>
2020-06-18 08:55:32 +0200

    updated component NewRecipe (features: adding new recipe to db.json) styles & logic

--- src/js/components/AddRecipe.js
+++ src/js/components/AddRecipe.js
@@ -11,7 +11,8 @@ export default class AddRecipe extends Component {
             step: "",
             steps: [],
             ingredient: "",
-            ingredients: []
+            ingredients: [],
+            warning: ""
         }
     }
 
@@ -23,15 +24,23 @@ export default class AddRecipe extends Component {
 
     handleHide = () => {
         this.setState({
-            isShow: false
+            isShow: false,
+            name: "",
+            description: "",
+            step: "",
+            steps: [],
+            ingredient: "",
+            ingredients: [],
+            warning: ""
         })
     }
 
     addToSteps = e => {
         e.preventDefault();
-        if(this.state.step) {
+        if (this.state.step) {
             this.setState({
-                steps: [...this.state.steps, this.state.step]
+                steps: [...this.state.steps, this.state.step],
+                step: ""
             })
         }
 
@@ -40,46 +49,59 @@ export default class AddRecipe extends Component {
     addToIngredients = e => {
         e.preventDefault();
         this.setState({
-            ingredients: [...this.state.ingredients, this.state.ingredient]
+            ingredients: [...this.state.ingredients, this.state.ingredient],
+            ingredient: ""
         })
     }
 
     postToBase = (e, name, description, steps, ingredients) => {
         e.preventDefault();
+        let newID;
+        fetch("http://localhost:3000/recipes/")
+        .then(res => res.json())
+        .then(data => newID = data.length)
         const data = {
-            "id": 1,
+            "id": newID,
             "name": name,
             "description": description,
             "steps": steps,
             "ingredients": ingredients
-        }
-        fetch("http://localhost:3000/recipes/", {
-            method: 'POST', // or 'PUT'
+        };
+        if(this.state.steps.length && this.state.ingredients.length) {
+            fetch("http://localhost:3000/recipes/", {
+            method: 'POST',
             headers: {
-                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
             },
             body: JSON.stringify(data),
         })
             .then(response => response.json())
             .then(data => {
                 console.log('Success:', data);
+                this.handleHide();
             })
             .catch((error) => {
                 console.error('Error:', error);
             });
+        } else {
+            this.setState({
+                warning: "Przepis musi zawierać co najmniej 1 składnik i 1 instrukcję!"
+            })
+        }
     }
 
     render() {
         return (
             <div className="modal">
                 {!this.state.isShow && <button className="modal__btn" onClick={this.handleShow}>
-                    <i class="far fa-plus-square"></i>
+                    <i className="far fa-plus-square"></i>
                     <span>Dodaj przepis</span>
                 </button>}
-                {this.state.isShow && <div className="modal__popup-add-recipe">
+                {this.state.isShow && <form className="modal__popup-add-recipe" onSubmit={e => this.postToBase(e, this.state.name, this.state.description, this.state.steps, this.state.ingredients)}>
                     <div className="modal__popup-add-recipe__header">
                         <h2>Nowy przepis</h2>
-                        <button className="modal__popup-add-recipe__header_btn" onClick={this.handleHide}>Zapisz i zamknij</button>
+                        <button type="submit" className="modal__popup-add-recipe__header_btn">Zapisz i zamknij</button>
                     </div>
                     <div className="modal__popup-add-recipe__new">
                         <div className="modal__popup-add-recipe__new__wrapper">
@@ -94,30 +116,31 @@ export default class AddRecipe extends Component {
                     <div className="modal__popup-add-recipe__add-list">
                         <div className="modal__popup-add-recipe__add-list__content">
                             <h3>instrukcje</h3>
-                            <form>
-                                <textarea maxlength="150" required value={this.state.step} onChange={e => this.setState({ step: e.target.value })} />
+                            <div className="modal__popup-add-recipe__add-list__content__container">
+                                <textarea maxlength="150" value={this.state.step} onChange={e => this.setState({ step: e.target.value })} />
                                 <button onClick={e => this.addToSteps(e)}><i className="fas fa-plus-square"></i></button>
-                            </form>
+                            </div>
                             <ol>
-                                {this.state.steps.map(step => {
-                                    return <li>{step}</li>
+                                {this.state.steps.map((step, id) => {
+                                    return <li key={id}><span>{step}</span> <i className="fas fa-edit"></i><i class="far fa-trash-alt"></i></li>
                                 })}
                             </ol>
                         </div>
                         <div className="modal__popup-add-recipe__add-list__content">
                             <h3>składniki</h3>
-                            <form>
-                                <textarea maxlength="50" required value={this.state.ingredient} onChange={e => this.setState({ ingredient: e.target.value })} />
+                            <div className="modal__popup-add-recipe__add-list__content__container">
+                                <textarea maxlength="50" value={this.state.ingredient} onChange={e => this.setState({ ingredient: e.target.value })} />
                                 <button onClick={e => this.addToIngredients(e)}><i className="fas fa-plus-square"></i></button>
-                            </form>
+                            </div>
                             <ul>
-                                {this.state.ingredients.map(ingredient => {
-                                    return <li>{ingredient}</li>
+                                {this.state.ingredients.map((ingredient, id) => {
+                                    return <li key={id}><span>{ingredient}</span> <i className="fas fa-edit"></i><i class="far fa-trash-alt"></i></li>
                                 })}
                             </ul>
                         </div>
                     </div>
-                </div>}
+                    <span className="modal__popup-add-recipe__warning">{this.state.warning}</span>
+                </form>}
             </div>
         )
     }
