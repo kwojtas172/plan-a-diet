6b412f64f0bac047f64bee5e79373f14535b1356
pepe199111 <piotrpustul1@gmail.com>
2020-06-24 00:26:38 +0100

    simply changes on develop + added amount recipes in widgets

--- src/js/components/LandingPage.js
+++ src/js/components/LandingPage.js
@@ -7,8 +7,6 @@ import FormToSubscription from "./FormToSubscription";
 import AboutAuthor from "./AboutAuthor";
 import Footer from "./Footer";
 
-// import WeeklyPreviewTable from './WeeklyPreviewTable';
-
 export default class LandingPage extends React.Component {
     render() {
         return (
@@ -20,7 +18,6 @@ export default class LandingPage extends React.Component {
                 <FormToSubscription />
                 <AboutAuthor />
                 <Footer />
-                {/* <WeeklyPreviewTable /> */}
             </div>
         )
     }
