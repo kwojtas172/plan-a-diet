465706a6f5a3a77d091b673f9df24a73866abe76
kwojtas172 <k.wojtas172@gmail.com>
2020-07-07 11:22:00 +0200

    Updates

---
+++ precache-manifest.5d576adc71f7f9fafd4d41b08d96d3fc.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "729ae6d6c43ec84e721a177ede0c58bb",
+    "url": "/plan-a-diet/index.html"
+  },
+  {
+    "revision": "555bb03a6d380849c2c7",
+    "url": "/plan-a-diet/static/css/2.240942cf.chunk.css"
+  },
+  {
+    "revision": "80de6efbe448a108b2b4",
+    "url": "/plan-a-diet/static/css/main.eaaaaa24.chunk.css"
+  },
+  {
+    "revision": "555bb03a6d380849c2c7",
+    "url": "/plan-a-diet/static/js/2.9dad3c8e.chunk.js"
+  },
+  {
+    "revision": "80de6efbe448a108b2b4",
+    "url": "/plan-a-diet/static/js/main.e7042e6e.chunk.js"
+  },
+  {
+    "revision": "111420073873e2d13dcd",
+    "url": "/plan-a-diet/static/js/runtime~main.c50c5c42.js"
+  },
+  {
+    "revision": "59706e22e75de7fa98f96205b230013f",
+    "url": "/plan-a-diet/static/media/about-app-1.59706e22.jpg"
+  },
+  {
+    "revision": "54e8d13985ecf8fcb67b9bf7c15e16df",
+    "url": "/plan-a-diet/static/media/about-app-2.54e8d139.jpg"
+  },
+  {
+    "revision": "257e88d91580f167d1feafb834df6a00",
+    "url": "/plan-a-diet/static/media/about-app-3.257e88d9.jpg"
+  },
+  {
+    "revision": "4c1ae61a1869fb09b0b310fb7390b456",
+    "url": "/plan-a-diet/static/media/about-author.4c1ae61a.jpg"
+  },
+  {
+    "revision": "4506b8ffdcbef2c7efd477194e80f46e",
+    "url": "/plan-a-diet/static/media/arrow-next.4506b8ff.svg"
+  },
+  {
+    "revision": "b9c5bb5a1a3e9e5c1f789103ccd85257",
+    "url": "/plan-a-diet/static/media/arrow-previous.b9c5bb5a.svg"
+  },
+  {
+    "revision": "baa58c3c16b76d6e733e5f658ec2a3f8",
+    "url": "/plan-a-diet/static/media/bcg-app.baa58c3c.png"
+  }
+]);
